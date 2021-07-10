92fbe3b3cc2572f342b529d775d2d728684e6179
lil-sahil <sahilnagpal93@yahoo.ca>
2021-07-10 19:06:34 -0400

    Screen, clear button logic added.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const screen = document.querySelector(".calculator-screen");
 const clear = document.querySelector('.clear');
 const posNeg = document.querySelector('.pos-neg');
 const percentage = document.querySelector('.percentage');
@@ -18,4 +19,46 @@ const add = document.querySelector('.add');
 const subtract = document.querySelector('.subtract');
 const equal = document.querySelector('.equal');
 
-equal.addEventListener('click', (e)=> console.log(e.target.innerText));
+
+let screenValue = 0;
+
+// Display pushed buttons on screen
+function displayOnScreen() {
+    document.querySelectorAll(".display").forEach( (event) => {
+    event.addEventListener('click', (e) => {
+        
+        // Max number of digits is 27
+        if (screen.childNodes.length === 27){
+            return false;
+        }
+
+        let tempDisp = document.createElement('h1');
+        screen.appendChild(tempDisp).textContent = e.target.innerText
+        });
+    });
+}
+
+displayOnScreen();
+
+
+// Clear screen whenever clear button or any of the operation buttons are pressed.
+// Also store the value from screen.
+
+function clearScreen() {
+    document.querySelectorAll('.operation-btn').forEach( (btn) => {
+        btn.addEventListener('click', () => {
+            getScreenValue();
+            Array.from(screen.childNodes).forEach(n => n.remove());
+        })
+    });
+}
+
+
+// Get the values stored in the H1 tags on the screen,
+
+function getScreenValue(){
+    return -1;
+}
+
+
+
